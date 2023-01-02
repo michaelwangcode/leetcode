041900f1de4f67ec4d2ebbc4b2a190a23e414dcc
michaelwangcode <michaelwangxv@gmail.com>
2023-01-02 02:58:00 -0500

    Add LeetCode Solutions

---
+++ Easy/To Lower Case.js
@@ -0,0 +1,28 @@
+//----- QUESTION ------//
+
+// Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+
+
+//----- SOLUTION -----//
+
+// Use the toLowerCase() function
+
+
+
+// Input: A string
+const s = "Hello";
+
+
+
+// This function returns a string in all lower case
+const toLowerCase = function(s) {
+
+  // Use the toLowerCase function
+  return s.toLowerCase();
+};
+
+
+
+// Test the toLowerCase function
+console.log(toLowerCase(s));
